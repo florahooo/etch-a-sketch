e83410f7ec80e7cce52afff14475e2955105069d
Flora Hu <floratllh@gmail.com>
2023-08-29 01:15:26 -0400

    added drawing and resetting capabilities

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const container = document.querySelector('.container');
+const inputBox = document.querySelector('.input-box')
+
+
 
 function makeGrid(number) {
 
@@ -7,17 +10,73 @@ function makeGrid(number) {
         for (y = 0; y < number; y++) {
 
             const gridElement = document.createElement('div');
+            const gridSize = 500;
+
 
             gridElement.classList.add('grid');
-            gridElement.style.width = '31.25px'
-            gridElement.style.height = '31.25px'
+            gridElement.style.width = `${gridSize/number}px`
+            gridElement.style.height = `${gridSize/number}px`
         
             container.append(gridElement);
 
         }
-       
-    
+    }
+
+}
+
+makeGrid(32)
+
+/*const gridBox = document.querySelectorAll('.grid');
+
+console.log(gridBox);
+
+gridBox.forEach((box) => {
+
+    box.addEventListener('click', epicClick());
+
+})
+
+function epicClick() {
+
+    console.log('hey hey hey')
+}*/
+
+
+function onHover() {
+
+    const gridBox = document.querySelectorAll('.grid');
+
+    for (i = 0; i < gridBox.length; i++) {
+
+        let currentBox = gridBox[i]
+
+        currentBox.addEventListener('mouseover', function() {
+            currentBox.style.backgroundColor= "black";
+        })
+
     }
 }
 
-makeGrid(16)
+onHover();
+
+const resetButton = document.querySelector('.reset-button');
+resetButton.addEventListener('click', resetBoard);
+
+function resetBoard() {
+
+    const gridBox = document.querySelectorAll('.grid');
+
+    for (i = 0; i < gridBox.length; i++) {
+
+        let currentBox = gridBox[i]
+        currentBox.style.backgroundColor= "whitesmoke";
+        }
+
+    }
+
+/*document.querySelector('input-form').addEventListener('submit', function(event){
+event.preventDefault();
+let userInput = document.getElementById('input').value; 
+alert('hello' + userInput);
+});*/
+
